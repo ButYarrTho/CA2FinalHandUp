8dc8dcc3081c9b2f0c51f4604080f5e37991a72c
Diggins <Dylanhabis@yahoo.com>
2024-04-28 17:40:13 +0100

    added logic to size, isEmpty, add, get, remove

--- src/main/java/org/example/LinkedList.java
+++ src/main/java/org/example/LinkedList.java
@@ -31,21 +31,65 @@ import org.w3c.dom.Node;
     }
 
     public boolean isEmpty() {
-       return false;
+       return size == 0;
     }
 
-    public void add(Appointment data) {
+    public void add(appoinments data) {
+      Node<appoinments> newNode = new Node<>(data);
+      if(isEmpty()) {
+         head = newNode;
+         tail = newNode;
+      }
+      else {
+         tail.next = newNode;
+         tail = newNode;
+      }
+      size++;
+
     }
 
-    public Appointment get(int index) {
+    public appoinments get(int index) {
 
-       return null;
+       if (index < 0 || index >= size)
+       {
+          throw new IndexOutOfBoundsException("Out of bounds");
+       }
+       Node<appoinments> current = head;
+       for (int i = 0; i < index; i++) {
+          current = current.next;
+       }
+       return current.data;
     }
 
-    public Appointment remove(int index) {
 
-       return null;
-    }
+
+    public appoinments remove(int index) {
+          if (index < 0 || index >= size) {
+             throw new IndexOutOfBoundsException("Index out of bounds");
+          }
+
+       appoinments DataToBeRemoved;
+          if (index == 0) {
+             DataToBeRemoved = head.data;
+             head = head.next;
+             if (head == null) {
+                tail = null;
+             }
+          } else {
+             Node<appoinments> before = head;
+             for (int i = 0; i < index - 1; i++) {
+                before = before.next;
+             }
+             DataToBeRemoved = before.next.data;
+             before.next = before.next.next;
+             if (before.next == null) {
+                tail = before;
+             }
+          }
+          size--;
+          return DataToBeRemoved;
+       }
+
 
     public void display()
     {
