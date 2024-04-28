cf8ecc24b582ad2e925e4deb240799ae2e380e48
AlexC <alexclinton133@gmail.com>
2024-04-28 18:51:06 +0100

    Added initial implementation of HashMap class with basic structure and constructors. Introduce generateKey method to HashMap class for generating unique keys based on a Patient object's first name, last name, and date of birth.

--- src/main/java/org/example/HashMap.java
+++ src/main/java/org/example/HashMap.java
@@ -1,4 +1,40 @@
 package org.example;
 
+/**
+ * HashMap is a data structure that stores key-value pairs. It provides constant-time performance for basic operations
+ * such as adding, removing, and retrieving elements.
+ */
 public class HashMap {
-}
+    private static final int DEFAULT_CAPACITY = 103;
+
+    private int size;
+    private int capacity;
+    private LinkedList<Patient>[] map;
+
+    public HashMap() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    /**
+     * Constructs a new HashMap with the specified capacity.
+     *
+     * @param capacity The initial capacity of the HashMap.
+     */
+    public HashMap(int capacity) {
+        this.capacity = capacity;
+        this.map = new LinkedList[capacity];
+        this.size = 0;
+    }
+
+    /**
+     * Generates a unique key for the given Patient object based on first name, last name, and date of birth.
+     *
+     * @param patient The Patient object for which the key is generated.
+     * @return The generated key.
+     */
+    private String generateKey(Patient patient) {
+        return patient.getFirstName() + patient.getLastName() + patient.getDateOfBirth();
+    }
+
+
+    }
