03f7b3e77a54543a43b1c7a2c4350898c39931b9
Diggins <Dylanhabis@yahoo.com>
2024-04-28 17:27:21 +0100

    added skeleton for Appointment and LinkedList

--- src/main/java/org/example/Appointment.java
+++ src/main/java/org/example/Appointment.java
@@ -1,4 +1,23 @@
 package org.example;
+import java.time.LocalDate;
 
 public class Appointment {
+
+    String patientFirstName;
+    String patientLastName;
+    LocalDate dateOfBirth;
+    String issue;
+    LocalDate date;
+    int triageLevel;
+    String doctorFullName;
+
+    public Appointment(String patientFirstName, String patientLastName, LocalDate dateOfBirth, String issue, LocalDate date, int triageLevel, String doctorFullName) {
+        this.patientFirstName = patientFirstName;
+        this.patientLastName = patientLastName;
+        this.dateOfBirth = dateOfBirth;
+        this.issue = issue;
+        this.date = date;
+        this.triageLevel = triageLevel;
+        this.doctorFullName = doctorFullName;
+    }
 }
--- src/main/java/org/example/LinkedList.java
+++ src/main/java/org/example/LinkedList.java
@@ -1,4 +1,54 @@
 package org.example;
 
-public class LinkedList {
-}
+import org.w3c.dom.Node;
+
+ class LinkedList<appoinments> {
+
+    private Node<appoinments> head;
+
+    private Node<appoinments> tail;
+
+    private int size;
+
+    private static class Node<N> {
+       N data;
+       Node<N> next;
+
+       Node(N data) {
+          this.data = data;
+          this.next = null;
+       }
+    }
+
+    public LinkedList() {
+       this.head = null;
+       this.tail = null;
+       this.size = 0;
+    }
+
+    public int size() {
+       return size;
+    }
+
+    public boolean isEmpty() {
+       return false;
+    }
+
+    public void add(Appointment data) {
+    }
+
+    public Appointment get(int index) {
+
+       return null;
+    }
+
+    public Appointment remove(int index) {
+
+       return null;
+    }
+
+    public void display()
+    {
+
+    }
+    }
