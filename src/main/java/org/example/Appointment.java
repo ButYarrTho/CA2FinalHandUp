c37a0c635cc95a1b6c8b249e56ce0e513f81703d
Diggins <Dylanhabis@yahoo.com>
2024-04-28 21:27:55 +0100

    Added getters and setters for Appointment to get and place data

--- src/main/java/org/example/Appointment.java
+++ src/main/java/org/example/Appointment.java
@@ -11,13 +11,6 @@ public class Appointment {
     int triageLevel;
     String docFullName;
 
-    public String getDocFullName() {
-        return docFullName;
-    }
-
-    public void setDocFullName(String docFullName) {
-        this.docFullName = docFullName;
-    }
 
     public Appointment(String patientFirstName, String patientLastName, LocalDate dateOfBirth, String issue, LocalDate date, int triageLevel, String docFullName) {
         this.patientFirstName = patientFirstName;
@@ -27,6 +20,61 @@ public class Appointment {
         this.date = date;
         this.triageLevel = triageLevel;
         this.docFullName = docFullName;
+    }
+    public String getDocFullName() {
+        return docFullName;
+    }
+
+    public void setDocFullName(String docFullName) {
+        this.docFullName = docFullName;
+    }
+
+    public String getPatientFirstName() {
+        return patientFirstName;
+    }
+
+    public void setPatientFirstName(String patientFirstName) {
+        this.patientFirstName = patientFirstName;
+    }
+
+    public String getPatientLastName() {
+        return patientLastName;
+    }
+
+    public void setPatientLastName(String patientLastName) {
+        this.patientLastName = patientLastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getIssue() {
+        return issue;
+    }
+
+    public void setIssue(String issue) {
+        this.issue = issue;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public int getTriageLevel() {
+        return triageLevel;
+    }
+
+    public void setTriageLevel(int triageLevel) {
+        this.triageLevel = triageLevel;
     }
 
 }
